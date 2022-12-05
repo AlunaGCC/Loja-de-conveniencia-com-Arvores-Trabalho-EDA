5db0d32b88b8ec6f6e33df2fdee17a19526c807e
AlunaGCC <56747633+AlunaGCC@users.noreply.github.com>
2022-12-04 23:54:40 -0300

    Assinatura das funções
    a

---
+++ loja.h
@@ -0,0 +1,39 @@
+/* CONJUNTO DE OPERACOES */
+
+
+/*--------------------------------*
+ * Controle de acesso login/senha *
+ *--------------------------------*/
+int acesso(){
+
+/*-------------------*
+ * Cadastrar produto *
+ *------------------*/
+int adastrar(){
+/*-----------------*
+ * Alterar produto *
+ *-----------------*/
+int alterar(){
+/*-----------------*
+ * Excluir produto *
+ *-----------------*/
+int excluir(){
+/*----------------*
+ * Buscar produto *
+ *----------------*/
+int buscarProduto(){
+/*----------------*
+ * Vender produto *
+ *----------------*/
+int vender(){
+/*-------------------*
+ * Relatorio por TIPO*
+ *-------------------*/
+int relatorioTipo(){
+/*--------------------*
+ * Relatorio de VENDA *
+ *--------------------*/
+int relatorioVenda(){
+
+
+
